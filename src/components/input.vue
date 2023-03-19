eeae84d8e407d2d811bfebf08b2aa12a4a4508e0
Leo <e-leokle@iskule.no>
2023-03-19 15:22:28 +0100

    nice

---
+++ src/components/input.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+export default {
+  props: ['modelValue', 'placeholder', 'type'],
+  emits: ['update:modelValue'],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue
+      },
+      set(value:any) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+    <input class="input" v-model="value" :placeholder="placeholder" :type="type" />
+    <div class="underline"></div>
+</div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    transition: 0.5s;
+}
+
+.input {
+    width: 100%;
+    opacity: 60%;
+    height: 2.5rem;
+    font-size: 2.25rem;
+    color: var(--green);
+    background-color: transparent;
+    border: none;
+    outline: none;
+}
+
+.underline {
+    width: 100%;
+    height: 5px;
+    opacity: 60%;
+    background-color: var(--green);
+    border-radius: 1rem;
+}
+
+.input:focus + .underline, .input:focus {
+    opacity: 100%;
+}
+
+.input::placeholder {
+  color: var(--green);
+}
+</style>
--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,15 +1,66 @@
+<script lang="ts">
+import Offer from "@/components/offer.vue"
+import Input from "@/components/input.vue"
+
+export default {
+  components: { Input },
+  data() {
+    return {
+      Fornavn: '',
+      Etternavn: '',
+      Alder: 0,
+      Epost: '',
+      Telefon: 0, 
+    }
+  },
+  methods: {
+    test () {
+      console.log(this.Fornavn)
+      console.log(this.Alder)
+    }
+  }
+}
+</script>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<form
+@submit.prevent="test"
+>
+  <h1>Kontaktinformasjon</h1>
+  <div class="personal-info">
+    <Input placeholder="Fornavn" v-model="Fornavn" type="text"/>
+    <Input placeholder="Fornavn" v-model="Etternavn" type="text"/>
+    <select v-model="Alder">
+      <option v-for="i in 100" :value="i">{{ i }} År</option>
+    </select>
   </div>
+
+  <button>Test</button>
+</form>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 2.25rem;
+  font-weight: 700;
+  color: var(--green);
+}
+
+.personal-info {
+  display: flex;
+  flex: 1 1 160px;
+  gap: 5.5rem;
 }
 </style>
