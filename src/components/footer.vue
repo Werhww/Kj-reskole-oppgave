b958e2c27844c17a35695a0968380ea46a359310
Leo <e-leokle@iskule.no>
2023-03-19 02:16:13 +0100

    comps

---
+++ src/components/footer.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+<footer>
+    <div class="content">
+        <div class="colume1">
+            <div>
+                <RouterLink to="/"><img src="../assets/logo.svg" alt="Logo" class="logo"></RouterLink>
+                <h1>Trafikkskole</h1>
+            </div>
+            <p>Rask & Tidig</p>
+            <a href="https://maps.app.goo.gl/5WPVkvrCbqeiDL6u8" target="_blank">Lungegårdskaien 40, 5015 Bergen</a>
+            <a href="tel:+4755189240">+47 55 18 92 40</a>
+            <a href="mailto:post@rask.no">post@rask.no</a>
+        </div>
+        <div class="colume2">
+            <a href="#topAnchor">Til Top</a>
+            <RouterLink to="/Bruker">Min side</RouterLink>
+            <RouterLink to="/omoss">Booking</RouterLink>
+        </div>
+    </div>
+</footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: var(--red);
+    color: white;
+    margin-top: 5rem;
+}
+
+.content {
+    display: flex;
+    justify-content: space-around;
+    align-items: start;
+    padding: 2rem 0;
+}
+
+/* Colume 1 */
+.colume1 {
+    display: flex;
+    flex-direction: column;
+    gap: .3rem;
+}
+
+.colume1 > div {
+    display: flex;
+    align-items: center;
+    gap: 0.688rem;
+}
+
+.logo {
+    height: 5rem;
+}
+
+.colume1 > div > h1 {
+    font-family: 'Iceberg', cursive;
+    padding-top: 2rem;
+}
+
+.colume1 > a  {
+    font-size: 1.5rem;
+    color: white;
+}
+
+.colume1 > p  {
+    font-size: 1.5rem;
+    font-weight: 300;
+}
+
+.colume2 {
+    display: flex;
+    flex-direction: column;
+    gap: .3rem;
+    padding-top: 2.5rem;
+}
+
+.colume2 a{
+    color: white;
+    font-size: 1.5rem;
+    text-decoration: none;
+}
+</style>
+
+<!-- Meida Querie -->
+<style scoped>
+@media only screen and (max-width: 600px) {
+    /* Colume 1 */
+    .logo {
+        height: 1.75rem;
+    }
+
+    .colume1 > div > h1 {
+        padding-top: 0.5rem;
+        font-size: 1rem;
+    }
+
+    .colume1 > a  {
+        font-size: 0.688rem;
+    }
+
+    .colume1 > p  {
+        font-size: 0.688rem;
+    }
+
+    .colume2 a{
+        font-size: 0.688rem;
+    }
+}
+</style>
---
+++ src/components/offer.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+export default {
+  props: ['imgLink', 'offer'],
+}
+</script>
+
+<template>
+<div class="tilbud-container">
+  <div><img v-bind:src="imgLink" :alt="offer"></div>
+  <p>{{ offer }}</p>
+</div>
+</template>
+
+<style scoped>
+.tilbud-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.tilbud-container > div {
+  background-color: #D9D9D9;
+  border-radius: 5rem;
+  width: 10.875rem;
+  height: 10.875rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tilbud-container > div > img{
+  width: 9.5rem;
+}
+
+.tilbud-container > p {
+  font-size: 1.5rem;
+}
+</style>
+
+<style scoped>
+@media only screen and (max-width: 600px) {
+
+  .tilbud-container > div {
+    width: 7.5rem;
+    height: 7.5rem;
+  }
+
+  .tilbud-container > div > img{
+    width: 6.875rem;
+  }
+
+  .tilbud-container > p {
+    font-size: 1rem;
+  }
+}
+</style>
