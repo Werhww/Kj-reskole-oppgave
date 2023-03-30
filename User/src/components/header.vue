3994f74160562b5638604588a9970173375aa1c2
Werhww <kleppeleo@gmail.com>
2023-03-30 22:37:54 +0200

    update

---
+++ User/src/components/header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+
+const props = defineProps<{
+    userName: string,
+}>()
+</script>
+
+<template>
+<header>
+    <router-link to="/chat"><img src="../assets/chat.svg"></router-link>
+    <p>{{ userName }}</p>
+</header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: flex-end;
+    align-items: end;
+    padding: 1.75rem 4.625rem;
+    gap: 2.125rem;
+}
+
+p {
+    font-size: 2.5rem;
+    padding: 0.625rem 4.375rem;
+    background-color: var(--red);
+    color: white;
+    font-family: 'Iceberg', cursive;
+}
+</style>
---
+++ User/src/views/Home.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+const placholder = {
+    gjenomførte: 5600,
+    fremtidige: 3400,
+    kjørtetimer: 20,
+    presentsjoner: ['Trafikkopplæringen', 'Trafikkopplæringen', 'Mennesket i trafikken']
+}
+</script>
+
+<template>
+<main>
+<div class="top">
+    <div>
+        <div class="cours_title title">
+            <div>
+                <H1>Kurs</H1>
+                <RouterLink to="/courses#list">List / Betaling</RouterLink>
+            </div>
+            <div></div> 
+        </div>
+        <div class="cours_content">
+            <span>
+                <p>Gjennomførte kurs</p>
+                <p>{{ placholder.gjenomførte }} kr</p>
+            </span>
+            <span>
+                <p>Fremtidig kurs</p>
+                <p>{{ placholder.fremtidige }} kr</p>
+            </span>
+            <span>
+                <p>Total</p>
+                <p>{{ placholder.gjenomførte + placholder.fremtidige }} kr</p>
+            </span>
+        </div>
+    </div>
+    <div>
+        <div class="achv_title title">
+            <div>
+                <H1>Gjennomført</H1>
+                <RouterLink to="/courses#achievements">Alle prestasjonene</RouterLink>
+            </div>
+            <div></div> 
+        </div>
+        <div class="achv_content">
+            <span>
+                <p>Kjørtetimer</p>
+                <p>{{ placholder.kjørtetimer }} timer</p>
+            </span>
+            <span><p v-for="item in placholder.presentsjoner">{{ item }}</p></span>
+        </div>
+    </div>
+</div>
+</main>
+</template>
+
+<style scoped>
+
+.achv_content span:last-child {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+
+.achv_content span:last-child p {
+    flex: 1 1 5rem;
+    color: var(--green);
+    text-align: center;
+}
+
+.cours_content, .achv_content{
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+    padding-top: 1.375rem;
+}
+
+.cours_content span, .achv_content span:first-child{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0  1.25rem  0  0.2rem;
+}
+
+.cours_content span p, .achv_content span p{
+    font-size: 1.5rem;
+}
+
+.cours_content span:last-child p{
+    font-weight: 700;
+}
+
+
+.top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.top div{
+    width: 30rem;
+}
+
+.cours_title, .cours_title a {
+    color: var(--red);
+}
+
+.cours_title > div:not(:first-child) {
+    height: 5px;
+    background-color: var(--red);
+    border-radius: 5rem;
+
+}
+
+.achv_title, .achv_title a {
+    color: var(--green);
+}
+
+.achv_title > div:not(:first-child) {
+    height: 5px;
+    background-color: var(--green);
+    border-radius: 5rem;
+} 
+
+.title {
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+}
+
+.title > div:not(:last-child) {
+    display: flex;
+    flex-direction: row;
+    align-items: end;
+    justify-content: space-between;
+}
+
+.title > div:not(:last-child) > h1 {
+    padding: 0  0.2rem;
+    font-size: 2.5rem;
+    font-weight: 700;
+}
+
+.title > div:not(:last-child) > a {
+    padding: 0  1.25rem;
+}
+
+
+</style>
