ad772f51a145123d6e3d7aecb81a724f8a1b7993
Leo <e-leokle@iskule.no>
2023-03-31 14:20:44 +0200

    update

---
+++ User/src/components/button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+    text: string;
+    color: string;
+}>()
+</script>
+
+<template>
+<div v-bind="$attrs">
+    <p>{{ text }}</p>
+</div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    background-color: v-bind(color);
+    padding: 0.375rem 4.125rem;
+}
+
+div p {
+    color: white;
+    font-size: 2rem;
+    font-weight: 700;
+}
+</style>
--- User/src/views/Courses.vue
+++ User/src/views/Courses.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
-
+import Button from "@/components/button.vue";
 </script>
 
 <template>
+<main>
+    <section>
+        <div class="kurs_top">
+            <div>
+                <H1>Kurs</H1>
+                <a id="list"></a>
+            </div>
+            <Button text="Tilbake" color="var(--red)"></Button>
+        </div>
+    </section>
+    <section>
 
+    </section>
+</main>
 </template>
 
 <style scoped>
-
+.kurs_top{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2rem;
+}
 </style>
