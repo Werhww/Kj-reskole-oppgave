d01ea1b6b2df213396e1ff57387c424a6c8e5ce1
Werhww <kleppeleo@gmail.com>
2023-04-20 16:42:50 +0200

    update

---
+++ Admin/src/components/studentlist.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+    name:string,
+    nextCourse:string,
+    studentID:string
+}>()
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
--- Admin/src/views/Home.vue
+++ Admin/src/views/Home.vue
@@ -1,11 +1,48 @@
 <script setup lang="ts">
-
+import Underline from '@/components/underline.vue';
 </script>
 
 <template>
-<p>hello</p>
+<main>
+    <section>
+        <div>
+            <h1>Min elever</h1>
+            <Underline color="var(--red)" />
+            <div class="students_info">
+                <p>Elev</p>
+                <p>Neste kurs</p>
+                <p>Hidden text</p><!-- This is hidden to even out teh bar -->
+            </div>
+        </div>
+    </section>
+</main>
 </template>
 
 <style scoped>
+h1 {
+    color: var(--red);
+    font-size: 2.5rem;
+    font-weight: 600;
+}
+</style>
+
+<!-- Students / top part -->
+<style setup>
+.students_info {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 0.5rem;
+}
+
+.students_info p {
+    font-size: 1.5rem;
+    font-weight: 500;
+}
+
+.students_info p:last-child {
+    /* Hidden element to even out info bar */
+    opacity: 0;
+    user-select: none;
+}
 
 </style>
