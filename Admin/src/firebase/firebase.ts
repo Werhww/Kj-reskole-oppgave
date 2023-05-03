cff081476ff3d22b5bdec93178e476250306b86b
Werhww <kleppeleo@gmail.com>
2023-05-03 10:38:10 +0200

    update

---
+++ Admin/src/firebase/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMxedZNDcj0Nj6AbhZ1HR_IsPOfhRN0Gk",
+  authDomain: "kjoreskole-525f2.firebaseapp.com",
+  projectId: "kjoreskole-525f2",
+  storageBucket: "kjoreskole-525f2.appspot.com",
+  messagingSenderId: "148579261458",
+  appId: "1:148579261458:web:7f55b3638afb97764c8ccb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+export {
+    db
+}
